530c8c33553e4188ea953d18a868f40f041ec27e
nobody <noemail@blah>
2014-09-16 07:42:49 +0000

    Introduce statement_ versions
    Add three new functions:
    
        statement_sysdatetime
        statement_sysdatetimeoffset
        statement_sysutcdatetime
    
    with the same semantics as the base versions, but stable across
    multiple extensions within a statement.
    
    An ExecutorStart_hook is used to clear the stored timestamp, and it
    is then set on first use of any of these functions, so the cost of
    this support to code that does not use it is negligible.

--- pg_sysdatetime--1.0.sql
+++ pg_sysdatetime--1.0.sql
@@ -27,3 +27,33 @@ VOLATILE;
 
 COMMENT ON FUNCTION sysdatetime()
 IS 'Return high precision timestamp for current local (TimeZone) time as timestamp';
+
+
+
+
+CREATE OR REPLACE FUNCTION statement_sysutcdatetime()
+RETURNS TIMESTAMP WITHOUT TIME ZONE
+LANGUAGE c
+AS 'pg_sysdatetime','pg_sysutcdatetime_statement'
+STABLE;
+
+COMMENT ON FUNCTION statement_sysutcdatetime()
+IS 'Return high precision timestamp for start of statement UTC time as timestamp';
+
+CREATE OR REPLACE FUNCTION statement_sysdatetimeoffset()
+RETURNS TIMESTAMP WITH TIME ZONE
+LANGUAGE c
+AS 'pg_sysdatetime','pg_sysdatetimeoffset_statement'
+STABLE;
+
+COMMENT ON FUNCTION statement_sysdatetimeoffset()
+IS 'Return high precision timestamp for start of statement UTC time as timestamptz';
+
+CREATE OR REPLACE FUNCTION statement_sysdatetime()
+RETURNS TIMESTAMP WITHOUT TIME ZONE
+LANGUAGE c
+AS 'pg_sysdatetime','pg_sysdatetime_statement'
+STABLE;
+
+COMMENT ON FUNCTION statement_sysdatetime()
+IS 'Return high precision timestamp for start of statement local (TimeZone) time as timestamp';
--- pg_sysdatetime.c
+++ pg_sysdatetime.c
@@ -3,6 +3,8 @@
 #include "fmgr.h"
 #include "utils/geo_decls.h"
 #include "utils/datetime.h"
+#include "executor/execdesc.h"
+#include "executor/executor.h"
 
 #if defined(WIN32)
 #define WIN32_LEAN_AND_MEAN
@@ -12,6 +14,14 @@
 #ifdef PG_MODULE_MAGIC
 PG_MODULE_MAGIC;
 #endif
+
+PGDLLEXPORT void _PG_init(void);
+PGDLLEXPORT void _PG_fini(void);
+PGDLLEXPORT void pg_sysdatetime_execstarthook(QueryDesc *queryDesc, int eflags);
+static TimestampTz GetCurrentTimestampHighres(void);
+
+static ExecutorStart_hook_type next_ExecutorStart_hook = NULL;
+static TimestampTz pg_sysdatetime_statementtimestamp = 0;
 
 #if defined(WIN32)
 
@@ -90,4 +100,79 @@ pg_sysdatetime(PG_FUNCTION_ARGS)
 	Timestamp ts = GetCurrentTimestampHighres();
 
 	return DirectFunctionCall1(timestamptz_timestamp, TimestampGetDatum(ts));
+}
+
+
+static TimestampTz
+GetStatementTimestampHighres()
+{
+	/*
+	 * If no statement timestamp has been grabbed for this statement,
+	 * collect one.
+	 *
+	 * This isn't set directly in the ExecutorStart_hook, partly to reduce
+	 * impact on code that doesn't use these functions and partly because of an
+	 * ordering problem when this extension isn't in local_ or shared_preload_libraries.
+	 *
+	 * The executor hook that clears the statement timeout is only installed after
+	 * the executor starts and sees that this function is referenced, then goes to load it.
+	 * So it's too late to clear the timestamp for that execution. We could always initialize
+	 * pg_sysdatetime_statementtimestamp at DLL-load time, but we'd still have to do this
+	 * branch unless we wanted to get the timestamp for all statements.
+	 */
+	if (pg_sysdatetime_statementtimestamp == 0)
+		pg_sysdatetime_statementtimestamp = GetCurrentTimestampHighres();
+	return pg_sysdatetime_statementtimestamp;
+}
+
+PG_FUNCTION_INFO_V1(pg_sysutcdatetime_statement);
+
+PGDLLEXPORT Datum
+pg_sysutcdatetime_statement(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_TIMESTAMP(GetStatementTimestampHighres());
+}
+
+PG_FUNCTION_INFO_V1(pg_sysdatetimeoffset_statement);
+
+PGDLLEXPORT Datum
+pg_sysdatetimeoffset_statement(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_TIMESTAMPTZ(GetStatementTimestampHighres());
+}
+
+PG_FUNCTION_INFO_V1(pg_sysdatetime_statement);
+
+PGDLLEXPORT Datum
+pg_sysdatetime_statement(PG_FUNCTION_ARGS)
+{
+	return DirectFunctionCall1(timestamptz_timestamp, TimestampGetDatum(GetStatementTimestampHighres()));
+}
+
+PGDLLEXPORT void
+pg_sysdatetime_execstarthook(QueryDesc *queryDesc, int eflags)
+{
+	/* 
+	 * Reset the statement timestamp. We don't grab a timestamp
+	 * here because it might not be required in this statement.
+	 */
+	pg_sysdatetime_statementtimestamp = 0;
+
+	if (next_ExecutorStart_hook != NULL)
+		(*next_ExecutorStart_hook)(queryDesc, eflags);
+	else
+		standard_ExecutorStart(queryDesc, eflags);
+}
+
+PGDLLEXPORT void
+_PG_init()
+{
+	next_ExecutorStart_hook = ExecutorStart_hook;
+	ExecutorStart_hook = &pg_sysdatetime_execstarthook;
+}
+
+PGDLLEXPORT void 
+_PG_fini()
+{
+	ExecutorStart_hook = next_ExecutorStart_hook;
 }
